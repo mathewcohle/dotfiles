dad3a61b3813538f6fc44567ce80fb3fd4360537
mathewcohle <mathewcohle@gmail.com>
2017-09-17 15:34:26 +0200

    Update setup.sh
    Forgotten changes during the furious time of replacing and removing
    vim plugins.

--- setup.sh
+++ setup.sh
@@ -30,15 +30,17 @@ ln -s dotfiles/zshrc .zshrc
 
 # Install vim packages
 cd dotfiles/vim/bundle
+git clone https://github.com/Valloric/YouCompleteMe.git
+git clone https://github.com/junegunn/fzf.vim.git
+git clone https://github.com/junegunn/fzf.git
+git clone https://github.com/itchyny/lightline.vim
+git clone https://github.com/scrooloose/nerdtree.git
 git clone --depth=1 https://github.com/vim-syntastic/syntastic.git
 git clone https://github.com/Chiel92/vim-autoformat.git
+git clone git://github.com/tpope/vim-dispatch.git
 git clone https://github.com/tpope/vim-fugitive.git
 git clone https://github.com/airblade/vim-gitgutter.git
-git clone https://github.com/tpope/vim-repeat.git
 git clone https://github.com/tpope/vim-surround.git
-git clone https://github.com/Valloric/YouCompleteMe.git
-git clone https://github.com/ctrlpvim/ctrlp.vim.git
-git clone https://github.com/scrooloose/nerdtree.git
 
 # Setup Ycm
 cd YouCompleteMe/
