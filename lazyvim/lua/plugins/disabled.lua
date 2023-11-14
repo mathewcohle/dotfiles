1cde6d1d2e60009f05bf5fd8a2122e8b77455b2b
Matej Čamaj <camaj@protonmail.com>
2023-11-14 21:11:01 +0100

    Remove neovim and lazyvim config

--- lazyvim/lua/plugins/disabled.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- disable trouble
-  { "folke/persistence.nvim", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "nvim-pack/nvim-spectre", enabled = false },
-}
--- nvim/lua/cmds.lua
+++
@@ -1,4 +0,0 @@
-vim.api.nvim_create_autocmd(
-  "FileType", {
-  pattern={"qf"},
-  command=[[nnoremap <buffer> <C-v> <C-w><Enter><C-w>L]]})
